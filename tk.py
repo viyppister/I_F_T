e90f7683704275d742ad11fbf18717b09eb4bfd4
viyppister <89063490+viyppister@users.noreply.github.com>
2021-08-18 14:02:53 +0200

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,66 @@
+import tkinter         # import de tkinter
+from tkinter import ttk , Entry , Frame
+import sqlite3
+fenetre = tkinter.Tk ()   # création de la fenêtre principale
+fenetre.geometry("600x600")
+#fenetre.iconbitmap("\home\jeanpi\Bureau\Travail_Python\ 19122020\ tatoum.ico")
+fenetre.title("CALCUL   I_F_T")
+fenetre['bg']='bisque' # couleur de fond
+fenetre.resizable(width=False, height=False)
+
+def tkquit():
+    fenetre.destroy()
+
+'''
+# Création de la requete  Uniquement 1 fois pour créer nom email tel ....ect
+req = "CREATE TABLE fichier(id INTEGER PRIMARY KEY AUTOINCREMENT, Identité TEXT NOT NULL, Tel TEXT NOT NULL, Email TEXT NOT NULL)"
+# Exécution de la requete
+cur.execute(req)
+# Envoyer la requete
+conn.commit()
+# Fermer la connexion
+conn.close
+'''
+def sauvegarde():
+    conn = sqlite3.connect('fichier.db')  # connection  à la bdd
+    cur = conn.cursor() # Créer un cursor
+    global nom_prenom
+    # Récupération des entrées et placement dans une variable
+    contenu_nom = nom_prenom.get()
+    contenu_Tel=Tel.get ()
+    contenu_Email=Email.get ()
+    # Insérer les données dans la BDD
+    cur.execute("Insert into fichier (Identité,Tel,Email) values (?,?,?)",(contenu_nom , contenu_Tel, contenu_Email)) # Insérer les données dans la         BDD
+    conn.commit() # Engager l'opération
+    conn.close() # Fermer la connexion
+# ...
+zone_texte = tkinter.Label (fenetre ,text = "Votre Nom et Prenom")
+# ...
+zone_texte.pack(padx=0, pady=50)            # on ajoute la zone_texte à la fenêtre principale
+
+nom_prenom = Entry(fenetre, width=50)
+nom_prenom.pack()
+nom_prenom.focus_force()     # placement du curseur dans la première entrée.
+# ...
+zone_texte = tkinter.Label (fenetre ,text = "Votre Numero de Tel")
+# ...
+zone_texte.pack ()            # on ajoute la zone_texte à la fenêtre principale
+
+Tel = Entry(fenetre, width=25)
+Tel.pack(padx=5, pady=5)
+# ...
+zone_texte = tkinter.Label (text = "Votre Email")
+# ...
+zone_texte.pack ()            # on ajoute la zone_texte à la fenêtre principale
+
+Email = Entry(fenetre, width=25)
+Email.pack(padx=5, pady=5)
+
+# Création du bouton de Sauvegarde
+bouton = tkinter.Button (text = "sauvegarder" , command = sauvegarde)
+bouton.pack()
+
+
+bouton2 = tkinter.Button(fenetre, text="Quitter", command=tkquit)
+bouton2.pack(padx=100 , pady=100)
+fenetre.mainloop ()       # on affiche enfin la fenêtre principale et on attend
